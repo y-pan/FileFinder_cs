2c44a9cb4ab730f1344069ac04b74bffea7c56c9
Yun Kui Pan <panyunkui@gmail.com>
2017-01-17 20:39:47 -0500

    Initial commit

---
+++ FileFinder/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileFinder
+{
+    public partial class FileFinder : Form
+    {
+        public FileFinder()
+        {
+            InitializeComponent();
+        }
+    }
+}
