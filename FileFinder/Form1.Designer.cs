e2af50f45fd14c93836a5cc6bae94eaf1d759dd2
Yun Kui Pan <panyunkui@gmail.com>
2017-01-19 00:32:46 -0500

    Next is to do filter

--- FileFinder/Form1.Designer.cs
+++ FileFinder/Form1.Designer.cs
@@ -28,20 +28,242 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.windowBox = new System.Windows.Forms.FlowLayoutPanel();
+            this.headBox1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.windowBox.SuspendLayout();
+            this.headBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(17, 3);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnAdd.Size = new System.Drawing.Size(40, 40);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Location = new System.Drawing.Point(63, 3);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnRemove.Size = new System.Drawing.Size(40, 40);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "-";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(57, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Title";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(185, 2);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(374, 30);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Path";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(567, 2);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 30);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Filter";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Location = new System.Drawing.Point(738, 2);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(163, 30);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Content";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Location = new System.Drawing.Point(909, 2);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 30);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Open";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox2.Location = new System.Drawing.Point(57, 37);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 26);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Tag = "1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox3.Location = new System.Drawing.Point(185, 37);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(374, 26);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Tag = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(5, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 30);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Id";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox4.Location = new System.Drawing.Point(567, 37);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(163, 26);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Tag = "1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(738, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(163, 28);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.Tag = "1";
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(909, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 35);
+            this.button1.TabIndex = 12;
+            this.button1.Tag = "1";
+            this.button1.Text = ">>";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label7.Location = new System.Drawing.Point(5, 34);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Tag = "1";
+            this.label7.Text = "1";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // windowBox
+            // 
+            this.windowBox.AutoSize = true;
+            this.windowBox.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.windowBox.Controls.Add(this.headBox1);
+            this.windowBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.windowBox.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.windowBox.Location = new System.Drawing.Point(0, 0);
+            this.windowBox.Name = "windowBox";
+            this.windowBox.Size = new System.Drawing.Size(1002, 712);
+            this.windowBox.TabIndex = 12;
+            // 
+            // headBox1
+            // 
+            this.headBox1.Controls.Add(this.flowLayoutPanel3);
+            this.headBox1.Controls.Add(this.btnAdd);
+            this.headBox1.Controls.Add(this.btnRemove);
+            this.headBox1.Location = new System.Drawing.Point(3, 3);
+            this.headBox1.Name = "headBox1";
+            this.headBox1.Size = new System.Drawing.Size(1002, 43);
+            this.headBox1.TabIndex = 10;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(8, 8);
+            this.flowLayoutPanel3.TabIndex = 10;
             // 
             // FileFinder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(618, 424);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1002, 712);
+            this.Controls.Add(this.windowBox);
+            this.MaximumSize = new System.Drawing.Size(1024, 768);
             this.Name = "FileFinder";
-            this.Text = "Form1";
+            this.Text = "FileFinder";
+            this.windowBox.ResumeLayout(false);
+            this.headBox1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.TableLayoutPanel thistblpRowContainer;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.FlowLayoutPanel windowBox;
+        private System.Windows.Forms.FlowLayoutPanel headBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
     }
 }
 
--- FileFinder/Form1.cs
+++ FileFinder/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,237 @@ namespace FileFinder
 {
     public partial class FileFinder : Form
     {
+        int seq;
+        int w1, w2, w3, w4, w5, w6, hh, hr;
+        Color hbg = Color.Blue;
+        Color fcl = Color.Yellow;
+        Font hf = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+
+        List<TextBox> npathList;
+        List<TextBox> nfilterList;
+        List<ComboBox> ncontentList;
+        List<FlowLayoutPanel> nrowList;
         public FileFinder()
         {
             InitializeComponent();
+            windowBox.AutoScroll = true;
+            windowBox.WrapContents = false;
+
+            nrowList = new List<FlowLayoutPanel>();
+            npathList = new List<TextBox>();
+            nfilterList = new List<TextBox>();
+            ncontentList = new List<ComboBox>();
+            
+
+            w1 = 20; w2 = 60; w3 = 240; w4 = 80; w5 = 180; w6 = 50;            
+            hh = 25; hr = 25;
+            seq = 0;
+
+            addHeader();
         }
+
+        private void addHeader() {
+            //1
+            Label hid = new Label();
+            hid.Text = "ID";
+            hid.Width = w1;
+            hid.TextAlign = ContentAlignment.MiddleCenter;
+            hid.BackColor = hbg;
+            hid.ForeColor = fcl;
+            hid.Font = hf;
+            hid.Tag = -1;
+            //2
+            Label htitle = new Label();
+            htitle.Text = "TITLE";
+            htitle.Width = w2;
+            htitle.TextAlign = ContentAlignment.MiddleCenter;
+            htitle.BackColor = hbg;
+            htitle.ForeColor = fcl;
+            htitle.Font = hf;
+            htitle.Tag = -1;
+            //3
+            Label hpath = new Label();
+            hpath.Text = "PATH";
+            hpath.Width = w3;
+            hpath.TextAlign = ContentAlignment.MiddleCenter;
+            hpath.BackColor = hbg;
+            hpath.ForeColor = fcl;
+            hpath.Font = hf;
+            hpath.Tag = -1;
+
+            //4
+            Label hfilter = new Label();
+            hfilter.Text = "FILTER";
+            hfilter.Width = w4;
+            hfilter.TextAlign = ContentAlignment.MiddleCenter;
+            hfilter.BackColor = hbg;
+            hfilter.ForeColor = fcl;
+            hfilter.Font = hf;
+            hfilter.Tag = -1;
+            //5
+            Label hcontent = new Label();
+            hcontent.Text = "CONTENT";
+            hcontent.Width = w5;
+            hcontent.TextAlign = ContentAlignment.MiddleCenter;
+            hcontent.BackColor = hbg;
+            hcontent.ForeColor = fcl;
+            hcontent.Font = hf;
+            hcontent.Tag = -1;
+            //6
+            Label hopen = new Label();
+            hopen.Text = "OPEN";
+            hopen.Width = w6;
+            hopen.TextAlign = ContentAlignment.MiddleCenter;
+            hopen.BackColor = hbg;
+            hopen.ForeColor = fcl;
+            hopen.Font = hf;
+            hopen.Tag = -1;
+            
+            FlowLayoutPanel hrow = new FlowLayoutPanel();
+            hrow.FlowDirection = FlowDirection.LeftToRight;
+            hrow.Width = windowBox.Width;
+            hrow.Tag = -1;
+            hrow.Controls.Add(hid);
+            hrow.Controls.Add(htitle);
+            hrow.Controls.Add(hpath);
+            hrow.Controls.Add(hfilter);
+            hrow.Controls.Add(hcontent);           
+            hrow.Controls.Add(hopen);
+            hrow.Height = hh;
+
+            windowBox.Controls.Add(hrow);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {                        
+            //1
+            Label nid = new Label();
+            nid.Text = seq.ToString();
+            nid.Width = w1;
+            nid.TextAlign = ContentAlignment.MiddleCenter;
+            nid.Tag = seq;
+            //2
+            TextBox ntitle = new TextBox();
+            ntitle.Width = w2;
+            ntitle.Tag = seq;
+            
+            //3
+            TextBox npath = new TextBox();
+            npath.Width = w3;
+            npath.Name = "path";
+            npath.Tag = seq;
+            npath.KeyDown += getContent;
+            //4
+            TextBox nfilter = new TextBox();
+            nfilter.Width = w4;
+            nfilter.Name = "filter";
+            nfilter.Tag = seq;
+            nfilter.KeyDown += filterContent;
+            //5
+            ComboBox ncontent = new ComboBox();
+            ncontent.Width = w5;
+            ncontent.Name = "content";
+            ncontent.Tag = seq;
+            //ncontent.SelectedIndexChanged += cbxContent_SelectedIndexChanged;
+            //6
+            Button nopen = new Button();
+            nopen.Width = w6;
+            nopen.Text = ">>";
+            nopen.Name = "open";
+            nopen.Tag = seq;
+            nopen.Click += btnOpen_Click;
+
+            FlowLayoutPanel nrow = new FlowLayoutPanel();
+            nrow.FlowDirection = FlowDirection.LeftToRight;
+            nrow.Width = windowBox.Width;       
+            nrow.Tag = seq;
+
+            nrow.Controls.Add(nid);
+            nrow.Controls.Add(ntitle);
+            nrow.Controls.Add(npath);
+            nrow.Controls.Add(nfilter);
+            nrow.Controls.Add(ncontent);
+            nrow.Controls.Add(nopen);
+            nrow.Height = hr;
+
+            npathList.Add(npath);
+            nfilterList.Add(nfilter);
+            ncontentList.Add(ncontent);
+            nrowList.Add(nrow);
+            
+            windowBox.Controls.Add(nrow);
+            
+            seq++;
+        }
+        private void filterContent(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter)
+            {
+                TextBox trigger = (TextBox)sender;
+                int index = (int)trigger.Tag;
+                String filter = trigger.Text;
+                MessageBox.Show(ncontentList[index].Items.ToString());
+                foreach (object it in ncontentList[index].Items)
+                {
+                    Console.WriteLine(it.ToString());
+                }
+            }
+        }
+        private void getContent(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) {
+                TextBox trigger = (TextBox)sender;
+
+                int index = (int)trigger.Tag;
+                String path = trigger.Text;
+
+                try
+                {
+                    string[] files = Directory.GetFileSystemEntries(path);
+                    ncontentList[index].Items.Clear();
+                    foreach (string f in files)
+                    {
+                        // to filter content ....
+                        ncontentList[index].Items.Add(Path.GetFileName(f));
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button trigger = (Button)sender;
+                int index = (int)trigger.Tag;
+                string name = ncontentList[index].SelectedItem.ToString();
+                string prefix = npathList[index].Text;
+                string path = prefix + "\\" + name;
+
+                if (Directory.Exists(path))
+                {
+                    // open 
+                    Process.Start("explorer.exe", path);
+
+                }
+                else if (File.Exists(path))
+                {
+                    // open notepad
+                    Process.Start("notepad++.exe", path);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
     }
 }
